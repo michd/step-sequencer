c5ef981259aa5e627a7ffc9765fee37088e9fcc3
Michael Duerinckx <duerinckxmichael@gmail.com>
2013-01-20 21:08:10 +0000

    Basic fixed 4 tracks sequencer going. Tempo settable, play, stop buttons functional.

---
+++ assets/js/sequencer.js
@@ -0,0 +1,110 @@
+(function ($, _) {
+
+  var al = '/content/audio/2002DANC/';
+
+  function audioInstance (wav) {
+    var audio = document.createElement('audio');
+    audio.src = wav;
+    audio.preload = "auto";
+    audio.autobuffer = "autobuffer";
+    return audio;
+  }
+
+  function syncInterval(beatsPerMinute) {
+    
+  }
+
+
+  $(document).ready(function () {
+    var
+      steps = [[], [], [], []],
+      players = [[], [], [], []],
+      currentStep = 0,
+      i,
+      playTimeout,
+      playing = false,
+      timing = {
+        bpm: 140,
+        stepInterval: 125,
+        lastTime: Date.now()
+      };
+
+    //init audio instances
+    for (i = 0; i < 16; i += 1) {
+      players[0].push(audioInstance(al + 'KIK_1.wav'));
+      players[1].push(audioInstance(al + 'CLAP.wav'));
+      players[2].push(audioInstance(al + 'HAT_1.wav'));
+      players[3].push(audioInstance(al + 'SN4.wav'));
+    }
+
+    function checkTiming() {
+      var
+        curTime = Date.now(),
+        timeDiff = curTime - timing.lastTime,
+        desiredTimeDiff = (60 * 1000) / (Math.max(timing.bpm, 1) * 4),
+        ratio = desiredTimeDiff / timing.stepInterval;
+
+      timing.stepInterval = desiredTimeDiff * ratio;
+    }
+
+    function setTempo(newTempo) {
+      var tempo = parseInt(newTempo, 10);
+      tempo = Math.round(
+        Math.max(40, Math.min(newTempo, 300))
+      );
+      timing.bpm = tempo;
+      return tempo;
+    }
+
+    function playStep() {
+      var i;
+      for (i = 0; i < steps.length; i += 1) {
+        if (steps[i][currentStep]) {
+          players[i][currentStep].currentTime = 0;
+          players[i][currentStep].play();
+        }
+      }
+
+      currentStep += 1;
+      if (currentStep > 15) { currentStep = 0; }
+
+      if (playing) {
+        playTimeout = setTimeout(playStep, timing.stepInterval);
+      }
+      checkTiming();
+    }
+
+    function reset () {
+      clearTimeout(playTimeout);
+      playing = false;
+      currentStep = 0;
+    }
+
+    function play () {
+      reset();
+      playing = true
+      timing.lastTime = Date.now()
+      playTimeout = setTimeout(playStep, timing.stepInterval);
+    }
+
+    $('tr .step').click(function () {
+      stepIndex = $(this).data('step');
+      $(this).toggleClass('on');
+      steps[$(this).closest('tr').data('track')][stepIndex] = $(this).hasClass('on');
+    });
+
+    $('#play').click(play);
+    $('#stop').click(reset);
+
+    $('#tempo').change(function() {
+      $(this).val(setTempo($(this).val()));
+    });
+
+
+
+
+
+
+
+  });
+}(window.jQuery, window._));
