7971a757d800e2c131c02b3f9fbd87c4320959cf
Michael Duerinckx <duerinckxmichael@gmail.com>
2013-02-02 20:57:20 +0000

    Added ui track component: removed premade table data in favour of dynamic, made a bunch event driven

--- assets/js/sequencer.js
+++ assets/js/sequencer.js
@@ -1,21 +1,18 @@
+/*global window: true, document: true*/
 (function (App, $) {
-
-  var al = '/content/audio/2002DANC/';
+  "use strict";
+  var
+    al = '/content/audio/2002DANC/',
+    events = App.eventDispatcher;
 
   $(document).ready(function () {
     var
       steps = [[], [], [], [], [], [], []],
       channels = [],
+      uiTracks = [],
       currentStep = 0,
+      $grid = $('#grid'),
       i;
-
-    channels[0] = new App.Channel(al + 'KIK_1.wav').setLabel('Kick');
-    channels[1] = new App.Channel(al + 'CLAP.wav').setLabel('Clap');
-    channels[2] = new App.Channel(al + 'HAT_7.wav').setLabel('Closed hihat');
-    channels[3] = new App.Channel(al + 'OP_HAT.wav').setLabel('Open hihat');
-    channels[4] = new App.Channel(al + 'RIDE_CYM.wav').setLabel('Ride').setVolume(0.4);
-    channels[5] = new App.Channel(al + 'SN_2.wav').setLabel('Snare');
-
 
     function playStep() {
 
@@ -26,16 +23,18 @@
           channels[i].trigger();
         }
       }
+      events.trigger('step.tick', currentStep, App);
 
       currentStep += 1;
+      //TODO needs to be more dynamic.
       if (currentStep > 15) { currentStep = 0; }
     }
 
-    function reset () {
+    function reset() {
       currentStep = 0;
     }
 
-    function play () {
+    function play() {
       reset();
       App.tempo.toggle(true);
     }
@@ -43,12 +42,6 @@
     // Link up tempo component with playStep
     App.Tempo(playStep);
 
-    // Toggle steps on or off
-    $('tr .step').click(function () {
-      stepIndex = $(this).data('step');
-      $(this).toggleClass('on');
-      steps[$(this).closest('tr').data('track')][stepIndex] = $(this).hasClass('on');
-    });
 
     $('#play').click(play);
     $('#stop').click(function () {
@@ -56,41 +49,42 @@
       App.tempo.toggle(false);
     });
 
-    $('#tempo').change(function() {
+    $('#tempo').change(function () {
       $(this).val(App.tempo.setBpm(parseInt($(this).val(), 10)).getBpm());
     });
 
-    // Toggle channels
-    $('tr th input[type=checkbox]').change(function() {
-      var
-        on = $(this).is(':checked'),
-        $tr = $(this).closest('tr');
 
-      channels[$tr.data('track')].toggle(on);
-      $tr.toggleClass('disabled', !on);
+    // Listen to UI for step toggles
+    events.subscribe('ui.step.toggled', function (channelId, stepIndex, on) {
+      steps[channelId][stepIndex] = on;
     });
 
-    //Allow changing volume
 
-    function updateVolume(value) {
-      var
-        $tr = $(this.$).closest('tr'),
-        newVol = value / 100;
-      channels[$tr.data('track')].setVolume(newVol);
+    // Listen to UI for channel toggles
+    events.subscribe('ui.track.toggled', function (channelId, on) {
+      channels[channelId].toggle(on);
+    });
+
+    // Listen to UI for volume changes
+    events.subscribe('ui.volume.changed', function (channelId, newVolume) {
+      channels[channelId].setVolume(newVolume);
+    });
+
+
+    // Set up initial audio channels
+    channels[0] = new App.Channel(al + 'KIK_1.wav');//.setLabel('Kick');
+    channels[1] = new App.Channel(al + 'CLAP.wav');//.setLabel('Clap');
+    channels[2] = new App.Channel(al + 'HAT_7.wav');//.setLabel('Closed hihat');
+    channels[3] = new App.Channel(al + 'OP_HAT.wav');//.setLabel('Open hihat');
+    channels[4] = new App.Channel(al + 'RIDE_CYM.wav');//.setLabel('Ride').setVolume(0.4);
+    channels[5] = new App.Channel(al + 'SN_2.wav');//.setLabel('Snare');
+
+    // Set up some mothereffing UI tracks
+    for (i = 0; i < channels.length; i += 1) {
+      uiTracks[i] = new App.ui.Track(i);
+      uiTracks[i].setLabel(channels[i].getLabel());
+      $grid.append(uiTracks[i].getRow());
     }
-
-    //Init jQuery Kontrol on volume inputs
-    $('tr .volume').dial({
-      flatMouse: true,
-      width: 45,
-      height: 25,
-      displayInput: true,
-      bgColor: '#CDA28F',
-      fgColor: '#240C00',
-      angleOffset: -90,
-      angleArc: 180,
-      change: updateVolume
-    })
 
   });
 }(window.STEPSEQUENCER, window.jQuery));
---
+++ assets/js/ui/track.js
@@ -0,0 +1,458 @@
+/*global window: true*/
+// user interface track module
+// Governs a single track (row) in the user interface
+(function (App, $, setTimeout) {
+  "use strict";
+
+  var events = App.eventDispatcher;
+
+
+  App.namespace('ui').Track = function (trackId) {
+
+
+    // ## Private properties
+
+    var
+      /**
+       * jQuery object containing the table row that is this track
+       * @type {jQuery}
+       */
+      $tr = $('<tr>', {'data-track': trackId}),
+
+      /**
+       * Array containing step html elements (individual step squares)
+       * @type {Array}
+       */
+      stepsCollection = [],
+
+      /**
+       * The number of steps to be displayed as a single beat (in one table cell)
+       * @type {Number}
+       */
+      stepsPerBeat = 4,
+
+      /**
+       * How many beat td's should be in one measure
+       * @type {Number}
+       */
+      beatsPerMeasure = 4,
+
+      /**
+       * How many measures should be displayed
+       * @type {Number}
+       */
+      measures = 1,
+
+      /**
+       * Label for this track
+       * @type {String}
+       */
+      trackName = 'Track #' + trackId,
+
+      /**
+       * Options for jQuery Kontrol's dial, used for the volume control
+       * @type {Object}
+       */
+      dialOptions = {
+        flatMouse: true,
+        width: 45,
+        height: 25,
+        displayInput: true,
+        bgColor: '#CDA28F',
+        fgColor: '#240C00',
+        angleOffset: -90,
+        angleArc: 180
+      },
+
+      /**
+       * Reference to self for in functions
+       * @type {App.ui.Track}
+       */
+      self = this;
+
+
+    // Force instantiation before continuing
+
+    if (this.constructor !== App.ui.Track) {
+      return new App.ui.Track(trackId);
+    }
+
+
+    // ## Private methods
+
+    /**
+     * Get the index value given a step html element
+     *
+     * @param  {HtmlElement} step
+     * @return {Number}
+     */
+    function getStepIndex(step) {
+      return stepsCollection.indexOf(step);
+    }
+
+
+    /**
+     * Ensures the stepCollection is at the correct number of steps
+     * The correct number of steps = stepsPerBeat * beatsPerMeasure * measures
+     *
+     * This is done by adding or removing steps to/from the array
+     *
+     */
+    function correctStepsCollection() {
+
+      var
+        stepsNeeded = stepsPerBeat * beatsPerMeasure * measures,
+        i, iMax;
+
+      // If we already have the right amount of steps, don't bother.
+      if (stepsNeeded === stepsCollection.length) { return; }
+
+      // Add or remove steps as appropriate
+      if (stepsNeeded >= stepsCollection.length) {
+
+        // Add the current mismatch number in steps
+        iMax = stepsNeeded - stepsCollection.length;
+        for (i = 0; i < iMax; i += 1) {
+          stepsCollection.push(
+            $('<div>', {'class': 'step'})[0]
+          );
+        }
+
+      } else {
+
+        // Remote the current mismatch number off the end of the array
+        // and from the tree.
+        iMax = stepsCollection.length - stepsNeeded;
+        for (i = 0; i < iMax; i += 1) {
+          $(stepsCollection.pop()).remove();
+        }
+      }
+    }
+
+
+    /**
+     * Redraws the row's contents to organize the steps, beats, and measures
+     * correctly.
+     */
+    function redraw() {
+
+      var
+        iSteps,
+        iBeats,
+        iMeasures,
+        evenMeasure,
+        stepIndex = 0,
+        $curTd;
+
+      //Ensure we have the correct number of steps to spread through the row
+      correctStepsCollection();
+
+      // Remove existing td before adding new ones
+      $tr.find('td').remove();
+
+      // Build structure inside table row
+      for (iMeasures = 0; iMeasures < measures; iMeasures += 1) {
+        evenMeasure = (iMeasures % 2 === 0);
+        for (iBeats = 0; iBeats < beatsPerMeasure; iBeats += 1) {
+
+          $curTd = $('<td>', {"class": (evenMeasure ? 'measure-even' : '')});
+
+          for (iSteps = 0; iSteps < stepsPerBeat; iSteps += 1) {
+            $curTd.append(stepsCollection[stepIndex]);
+            stepIndex += 1;
+          }
+
+          $tr.append($curTd);
+        }
+      }
+
+    }
+
+
+    /**
+     * Trigger volume changed event
+     *
+     * @param {Number} value
+     */
+    function updateVolume(value) {
+      events.trigger(
+        'ui.volume.changed', // Event name
+        [trackId, value / 100], // Data
+        $tr.find('th .volume')[0] //Contect
+      );
+    }
+
+
+    /**
+     * Initializes the table row for the first time
+     *
+     * Sets up the th with toggle, volume control and label for the first time,
+     * Sets up the steps divided over beats and measures through redraw()
+     *
+     * @todo Switch to using a template instead of ugly jQuery element creation
+     * @todo (low priority) allow changing the track name
+     */
+    function initRow() {
+
+      dialOptions.change = updateVolume;
+
+      //set up row heading
+      $tr.html(
+        $('<th>').append(
+          $('<input>', {type: 'checkbox', checked: 'checked'}),
+          $('<input>', {type: 'text', 'class': 'volume', value: 100})
+            .dial(dialOptions),
+          $('<label>').html(trackName)
+        )
+      );
+
+      redraw();
+    }
+
+
+    // Internal UI event listeners using jQuery
+
+    // Step clicked
+    $tr.on('click', '.step', function () {
+
+      var on = $(this).toggleClass('on').hasClass('on');
+
+      // Trigger event on main event dispatcher
+      events.trigger(
+        'ui.step.toggled', // Event name
+        [trackId, getStepIndex(this), on], // Params
+        this // Context
+      );
+
+    });
+
+
+    // Checkbox toggled
+    $tr.on('change', 'th input[type=checkbox]', function () {
+
+      var on = $(this).is(':checked');
+
+      $tr.toggleClass('disabled', !on);
+
+      events.trigger(
+        'ui.track.toggled', // Event name
+        [trackId, on], // Params
+        this // Context
+      );
+
+    });
+
+
+    $tr.on('click', 'th label', function () {
+
+      var oldValue = $(this).html();
+
+      $(this).replaceWith(
+        $('<input>', {type: 'text', value: $(this).html(), autofocus: 'autofocus'})
+          .keypress(function (event) {
+
+            // commit
+            if (event.keyCode === 13) {
+              $(this).replaceWith($('<label>').html($(this).val()));
+              events.trigger('ui.label.updated', [trackId, $(this).val()]);
+            }
+
+            // cancel
+            if (event.keyCode === 27) {
+              $(this).replaceWith($('<label>').html(oldValue));
+            }
+          })
+          .blur(function (event) {
+            $(this).replaceWith($('<label>').html(oldValue));
+          })
+      );
+    });
+
+
+    // ## Public interface methods
+
+    /**
+     * Updates how many steps are displayed as part of one beat
+     *
+     * @param {Number} newStepsPerBeat
+     */
+    this.setStepsPerBeat = function (newStepsPerBeat) {
+
+      if (typeof newStepsPerBeat !== 'number') {
+        throw new TypeError(
+          'setStepsPerBeat: newStepsPerBeat should of type Number, ' +
+            typeof newStepsPerBeat + ' given.'
+        );
+      }
+
+      stepsPerBeat = Math.round(newStepsPerBeat);
+      redraw();
+      return self;
+    };
+
+
+    /**
+     * Updates how many beats one measure consists of (for display)
+     *
+     * @param {Number} newBeatsPerMeasure
+     */
+    this.setBeatsPerMeasure = function (newBeatsPerMeasure) {
+
+      if (typeof newBeatsPerMeasure !== 'number') {
+        throw new TypeError(
+          'setBeatsPerMeasure: newBeatsPerMeasure should of type Number, ' +
+            typeof newBeatsPerMeasure + ' given.'
+        );
+      }
+
+      beatsPerMeasure = Math.round(newBeatsPerMeasure);
+      redraw();
+      return self;
+    };
+
+
+    /**
+     * Updates how many measures should be displayed
+     * @param {Number} newMeasures
+     */
+    this.setMeasures = function (newMeasures) {
+
+      if (typeof newMeasures !== 'number') {
+        throw new TypeError(
+          'setBeatsPerMeasure: newMeasures should of type Number, ' +
+            typeof newMeasures + ' given.'
+        );
+      }
+
+      measures = Math.round(newMeasures);
+      redraw();
+      return self;
+    };
+
+
+    /**
+     * Updates the label for this track
+     *
+     * @param {String} newLabel
+     * @return {App.ui.Track} self
+     */
+    this.setLabel = function (newLabel) {
+      $tr.find('th label').html(newLabel);
+      return self;
+    };
+
+    /**
+     * Get the jQuery row object for this track
+     *
+     * @return {jQuery}
+     */
+    this.getRow = function () {
+      return $tr;
+    };
+
+
+    //Initialize instance
+    (function () {
+
+      var
+
+        //Event response methods
+
+        /**
+         * Briefly highlight current step in the track.
+         * If this step is active (plays its sample), briefly highlight the channel
+         * name
+         *
+         * Triggered by step.tick event
+         *
+         * @param {Number} stepIndex
+         */
+        stepTick = function (stepIndex) {
+
+          var
+            $step = $(stepsCollection[stepIndex]),
+            on = $step.hasClass('on');
+
+          $tr.toggleClass('current', on);
+
+          $step.addClass('triggered');
+
+          setTimeout(function () {
+
+            $step.addClass('flash').removeClass('triggered');
+
+            if (on) {
+              $tr.addClass('flash').removeClass('triggered');
+            }
+
+          });
+        },
+
+
+        /**
+         * Updates the step component in the UI with the new on status, only if the
+         * context of this event is not the actual step element.
+         *
+         * This allows for dynamically loading patterns
+         *
+         * @param {Number} stepIndex
+         * @param {Boolean} on Whether the step is on in its new state
+         */
+        stepToggled = function (stepIndex, on) {
+
+          // Make sure event does not originate from a step
+          if (getStepIndex(this) === -1) {
+            $(stepsCollection[stepsCollection]).toggleClass('on', on);
+          }
+        },
+
+
+        /**
+         * Updates the disabled/enabled state of the track after making sure the
+         * event did not originate here.
+         *
+         * @param  {Number} channelId To make sure it's relevant to this
+         * @param  {Boolean} on
+         */
+        channelToggled = function (on) {
+
+          var trackCheckbox = $tr.find('th input[type=checkbox]')[0];
+
+          // Make sure the event doesn't originate from here
+          if (trackCheckbox === this) { return; }
+
+          $(trackCheckbox).prop('checked', on); // (Un)check checkbox
+
+          $tr.toggleClass('disabled', !on); // Enable or disable the row
+        },
+
+        /**
+         * Update volume input value if external volume change comes through
+         *
+         * @param  {Number} value [description]
+         */
+        volumeChanged = function (value) {
+
+          var volumeInput = $tr.find('th .volume')[0];
+
+          // We triggered this, so ignore
+          if (this === volumeInput) { return; }
+
+          $(volumeInput).val(Math.round(value * 100));
+
+        };
+
+      initRow();
+
+      // External event listeners
+
+      events.subscribe('step.tick', stepTick);
+      events.subscribe('step.toggled.channel-'    + trackId, stepToggled);
+      events.subscribe('channel.toggled.channel-' + trackId, channelToggled);
+      events.subscribe('volume.change.channel-'   + trackId, volumeChanged);
+
+    }()); // End intialize
+
+  };
+
+}(window.STEPSEQUENCER, window.jQuery, window.setTimeout));
