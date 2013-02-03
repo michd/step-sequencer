a3b2bc4ed044d4fd04cd1968d4456ed26e64b34c
Michael Duerinckx <duerinckxmichael@gmail.com>
2013-02-03 12:13:33 +0000

    Add list of available samples, implement replace sample dialog

--- assets/js/sequencer.js
+++ assets/js/sequencer.js
@@ -3,7 +3,57 @@
   "use strict";
   var
     al = '/content/audio/2002DANC/',
-    events = App.eventDispatcher;
+    events = App.eventDispatcher,
+    sampleLibrary = [
+      {label: "CLAP",     value: al + "CLAP.wav"},
+      {label: "CL_HAT_2", value: al + "CL_HAT_2.wav"},
+      {label: "CYM_REV",  value: al + "CYM_REV.wav"},
+      {label: "FX_1",     value: al + "FX_1.wav"},
+      {label: "HAT_1",    value: al + "HAT_1.wav"},
+      {label: "HAT_2",    value: al + "HAT_2.wav"},
+      {label: "HAT_3",    value: al + "HAT_3.wav"},
+      {label: "HAT_5",    value: al + "HAT_5.wav"},
+      {label: "HAT_6",    value: al + "HAT_6.wav"},
+      {label: "HAT_7",    value: al + "HAT_7.wav"},
+      {label: "KIK_10",   value: al + "KIK_10.wav"},
+      {label: "KIK_14",   value: al + "KIK_14.wav"},
+      {label: "KIK_1",    value: al + "KIK_1.wav"},
+      {label: "KIK_2",    value: al + "KIK_2.wav"},
+      {label: "KIK_3",    value: al + "KIK_3.wav"},
+      {label: "KIK_4",    value: al + "KIK_4.wav"},
+      {label: "KIK5",     value: al + "KIK5.wav"},
+      {label: "KIK_7",    value: al + "KIK_7.wav"},
+      {label: "KIK_8",    value: al + "KIK_8.wav"},
+      {label: "KIK_9",    value: al + "KIK_9.wav"},
+      {label: "KK6",      value: al + "KK6.wav"},
+      {label: "OP_HA_8",  value: al + "OP_HA_8.wav"},
+      {label: "OP_HAT_2", value: al + "OP_HAT_2.wav"},
+      {label: "OP_HAT_3", value: al + "OP_HAT_3.wav"},
+      {label: "OP_HAT_4", value: al + "OP_HAT_4.wav"},
+      {label: "OP_HAT_5", value: al + "OP_HAT_5.wav"},
+      {label: "OP_HAT_6", value: al + "OP_HAT_6.wav"},
+      {label: "OP_HAT_7", value: al + "OP_HAT_7.wav"},
+      {label: "OP_HAT_8", value: al + "OP_HAT_8.wav"},
+      {label: "OP_HAT",   value: al + "OP_HAT.wav"},
+      {label: "RIDE_CYM", value: al + "RIDE_CYM.wav"},
+      {label: "RIM__2",   value: al + "RIM__2.wav"},
+      {label: "RIM",      value: al + "RIM.wav"},
+      {label: "SHAKER_2", value: al + "SHAKER_2.wav"},
+      {label: "SHAKER_3", value: al + "SHAKER_3.wav"},
+      {label: "SHAKER",   value: al + "SHAKER.wav"},
+      {label: "SN_10",    value: al + "SN_10.wav"},
+      {label: "SN11",     value: al + "SN11.wav"},
+      {label: "SN_12",    value: al + "SN_12.wav"},
+      {label: "SN_1",     value: al + "SN_1.wav"},
+      {label: "SN_2",     value: al + "SN_2.wav"},
+      {label: "SN_3",     value: al + "SN_3.wav"},
+      {label: "SN4",      value: al + "SN4.wav"},
+      {label: "SN_5",     value: al + "SN_5.wav"},
+      {label: "SN__6",    value: al + "SN__6.wav"},
+      {label: "SN_7",     value: al + "SN_7.wav"},
+      {label: "SN_8",     value: al + "SN_8.wav"},
+      {label: "SN_9",     value: al + "SN_9.wav"}
+    ];
 
   $(document).ready(function () {
     var
@@ -68,6 +118,38 @@
     // Listen to UI for volume changes
     events.subscribe('ui.volume.changed', function (channelId, newVolume) {
       channels[channelId].setVolume(newVolume);
+    });
+
+    // Listen to UI for sample replace requests
+    events.subscribe('ui.sample.replace.requested', function (channelId, trackLabel) {
+
+      var
+        channel = channels[channelId],
+        currentSampleUrl = channel.getSampleUrl();
+
+      App.ui.dialogs.MultipleChoice({
+        content:     'Replace sample #' + (channelId + 1) + ' for ' + trackLabel,
+        choices:     sampleLibrary,
+        cancellable: true,
+
+        onChange: function (testSample) {
+          channel.setSample(testSample);
+        },
+
+        onClose: function (result) {
+          if (result === false) {
+            channel.setSample(currentSampleUrl);
+          } else {
+            channel.setSample(result);
+            events.trigger(
+              'sample.changed.channel-' + channelId,
+              [result, channel.getLabel()],
+              App
+           );
+          }
+        }
+
+      }).spawn();
     });
 
 
